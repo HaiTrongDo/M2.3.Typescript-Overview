4b7d0ccd1348f86370c3f9e0ebe5c5437a8a4e74
Hai Trong  Do <Hai Do>
2022-04-27 08:22:06 +0700

    staff class

---
+++ 3.1[Thực hành] Xây dựng lớp nhân viênAssignment/xay-dung-lop-nhan-vien.js
@@ -0,0 +1,32 @@
+var Staff = /** @class */ (function () {
+    function Staff(name, email, age) {
+        this.name = name;
+        this.email = email;
+        this._age = age;
+    }
+    Staff.prototype.getName = function () {
+        return this.name;
+    };
+    Staff.prototype.setName = function (name) {
+        this.name = name;
+    };
+    Staff.prototype.getEmail = function () {
+        return this.email;
+    };
+    Staff.prototype.setEmail = function (email) {
+        this.email = email;
+    };
+    Staff.prototype.getAge = function () {
+        return this._age;
+    };
+    Staff.prototype.setAge = function (age) {
+        this._age = age;
+    };
+    return Staff;
+}());
+var staff = new Staff("staff1", "staff@mail.com", 20);
+var nameStaff = staff.getName();
+console.log(nameStaff);
+staff.setName("Staff2");
+var currentNameStaff = staff.getName();
+console.log(currentNameStaff);
